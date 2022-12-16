47e783e6bc0824709020d603bd61be00461c5d29
daniel kenzo <daniel.kenzo@2rpnet.com>
2022-12-16 11:04:29 -0300

    add pokemons_oldschool

---
+++ pyspark/pokemons_oldschool.py
@@ -0,0 +1,19 @@
+import pyspark.sql.functions as F
+from pyspark.sql.window import Window
+from pyspark.sql import DataFrame
+import datetime
+import pandas as pd
+
+gen = spark.read.csv(path="hdfs:/user/2rp-danielk/.scratchdir/generation.csv", inferSchema=True, header=True,sep=',')
+poke = spark.read.csv(path="hdfs:/user/2rp-danielk/.scratchdir/pokemon.csv", inferSchema=True, header=True)
+genf=gen.select(F.col('*')).filter(F.col('date_introduced') < '2002-09-28')
+genf=genf.cache()
+pokemons=poke.join(genf,["generation"],'inner')
+pokemons.write.saveAsTable('work_dataeng.pokemons_oldschool_danielk', format="orc", mode="overwrite")
+
+
+
+
+
+
+
